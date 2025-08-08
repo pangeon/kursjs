bf9c672ac7e56b5266053925c873e3470ddac446
pangeon <kamil.cecherz@gmail.com>
2025-08-08 20:17:01 +0200

    ex_1

---
+++ app.js
@@ -0,0 +1,29 @@
+console.log("Witaj");
+
+const PI = 3.14;
+const planet = "Ziemia";
+const bad_number = 13;
+const yes = true;
+const not = false;
+const poland = Symbol('PL');
+
+const date = new Date();
+
+const empty = null;
+const secret = undefined;
+const no_number = NaN;
+const math_inf = Infinity;
+
+console.log(PI);
+console.log(planet);
+console.log(bad_number);
+console.log(yes);
+console.log(not);
+console.log(poland);
+
+console.log(date);
+
+console.log(empty);
+console.log(secret);
+console.log(no_number);
+console.log(math_inf);
